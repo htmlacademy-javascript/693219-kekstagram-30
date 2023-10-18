aac03e5abdd9547cd4ae763696999ca36fbe038a
Stan61rus <37905051+Stan61rus@users.noreply.github.com>
2023-10-18 14:09:13 +0300

    Fix

--- js/pictures.ts
+++ js/pictures.ts
@@ -2,8 +2,9 @@ import { generatePhotosArray } from './mock';
 import { Photo } from './mock';
 
 const picturesElement = document.querySelector<HTMLElement>('.pictures');
-const pictureFragment: HTMLTemplateElement | null =
-  document.querySelector('#picture');
+const pictureFragment = document
+  .querySelector<HTMLTemplateElement>('#picture')
+  ?.content.querySelector<HTMLElement>('.picture');
 
 if (!picturesElement || !pictureFragment) {
   throw new Error('Не удалось найти элементы в DOM');
@@ -15,18 +16,18 @@ const picturesListFragment: DocumentFragment =
   document.createDocumentFragment();
 
 similarPhotos.forEach(({ comments, description, likes, url }: Photo) => {
-  const pictureElement = pictureFragment.content.cloneNode(true) as HTMLElement;
+  const pictureElement = pictureFragment.cloneNode(true) as HTMLElement;
 
   if (!pictureElement) {
     throw new Error('Не удалось клонировать элемент');
   }
 
-  const pictureImg: HTMLImageElement | null =
-    pictureElement.querySelector('.picture__img');
-  const pictureLikes: HTMLElement | null =
-    pictureElement.querySelector('.picture__likes');
-  const pictureComments: HTMLElement | null =
-    pictureElement.querySelector('.picture__comments');
+  const pictureImg =
+    pictureElement.querySelector<HTMLImageElement>('.picture__img');
+  const pictureLikes =
+    pictureElement.querySelector<HTMLElement>('.picture__likes');
+  const pictureComments =
+    pictureElement.querySelector<HTMLElement>('.picture__comments');
 
   if (!pictureImg || !pictureLikes || !pictureComments) {
     throw new Error('Не удалось найти элементы в клонированном элементе');
